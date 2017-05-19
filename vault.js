86bf170785f741cbe91f79eee208b5657745f038
Seolhee Kim <seolhee@upspringmedia.com>
2017-05-18 19:52:41 -1000

    done

--- vault.js
+++ vault.js
@@ -1,4 +1,26 @@
 'use strict';
 module.exports = function() {
-  
+
+var vault = {};
+
+  function setValue(key, value){
+    vault[key] = value;
+    return key;
+
+    }
+
+  function getValue(key){
+    if(key === undefined){
+      return null;
+    }else if(!(key in vault)){
+      return null;
+    }else{
+      return vault[key];
+    }
+  }
+  return {
+    setValue: setValue,
+    getValue: getValue
+  }
+
 };
